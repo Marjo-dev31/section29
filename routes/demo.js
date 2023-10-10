7701f97e35742c887b1c51cb380a54ec7f3e7fa3
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2023-10-10 18:03:41 +0200

    error

--- routes/demo.js
+++ routes/demo.js
@@ -10,7 +10,20 @@ router.get('/', function (req, res) {
 });
 
 router.get('/signup', function (req, res) {
-  res.render('signup');
+  let sessionInputData = req.session.inputData;
+
+  if (!sessionInputData) {
+    sessionInputData = {
+      hasError: false,
+      email: '',
+      confirmEmail: '',
+      password: ''
+    };
+  }
+
+  req.session.inputData = null;
+
+  res.render('signup', { inputData: sessionInputData });
 });
 
 router.get('/login', function (req, res) {
@@ -32,7 +45,19 @@ router.post('/signup', async function (req, res) {
     !enteredEmail.includes('@')
   ) {
     console.log('Incorrect data');
-    return res.redirect('/signup');
+
+    req.session.inputData = {
+      hasError: true,
+      message: 'Invalid input - Please check your data.',
+      email: enteredEmail,
+      confirmEmail: enteredConfirmEmail,
+      password: enteredPassword
+    };
+
+    req.session.save(function () {
+      res.redirect('/signup');
+    });
+    return;
   };
 
   const existingUser = await db.getDb().collection('users').findOne({ email: enteredEmail });
@@ -87,6 +112,10 @@ router.get('/admin', function (req, res) {
   res.render('admin');
 });
 
-router.post('/logout', function (req, res) { });
+router.post('/logout', function (req, res) {
+  req.session.user = null;
+  req.session.isAuthenticated = false;
+  res.redirect('/');
+});
 
 module.exports = router;
