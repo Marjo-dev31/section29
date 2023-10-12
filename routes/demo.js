597b65b67c92edfa4d889b52cd22b80d11a0a290
Marjo-dev31 <marjory.bravo.dev@gmail.com>
2023-10-12 17:26:28 +0200

    practice

--- routes/demo.js
+++ routes/demo.js
@@ -44,8 +44,6 @@ router.post('/signup', async function (req, res) {
     enteredEmail !== enteredConfirmEmail ||
     !enteredEmail.includes('@')
   ) {
-    console.log('Incorrect data');
-
     req.session.inputData = {
       hasError: true,
       message: 'Invalid input - Please check your data.',
@@ -66,9 +64,20 @@ router.post('/signup', async function (req, res) {
   console.log(existingUser)
 
   if (existingUser) {
-    console.log('User already exist!');
-    return res.redirect('/signup');
-  }
+    // console.log('User already exist!');
+    req.session.inputData = {
+      hasError: true,
+      message: 'User exists already!',
+      email: enteredEmail,
+      confirmEmail: enteredConfirmEmail,
+      password: enteredPassword
+    };
+
+    req.session.save(function () {
+    res.redirect('/signup');
+  });
+return;
+};
 
   const hashedPassword = await bcrypt.hash(enteredPassword, 12); /*crypte en 12 caractères*/
 
@@ -89,16 +98,35 @@ router.post('/login', async function (req, res) {
   const existingUser = await db.getDb().collection('users').findOne({ email: enteredEmail });
 
   if (!existingUser) {
-    console.log('Could not log in!');
-    return res.redirect('/login');
-  }
+    req.session.inputData = {
+      hasError: true,
+      message: 'Could not log you in',
+      email: enteredEmail,
+      confirmEmail: enteredConfirmEmail,
+      password: enteredPassword
+    };
+
+    req.session.save(function() {
+    res.redirect('/login');
+  });
+  return;
+}
 
   const passwordsAreEgal = await bcrypt.compare(enteredPassword, existingUser.password); /*compar en decryptant*/
 
 
   if (!passwordsAreEgal) {
-    console.log('Could not log on - passwords are not egal!');
-    return res.redirect('/login');
+    req.session.inputData = {
+      hasError: true,
+      message: 'Could not log you in',
+      email: enteredEmail,
+      confirmEmail: enteredConfirmEmail,
+      password: enteredPassword
+    }
+    req.session.save(function() {
+      res.redirect('/login');
+    });
+    return;
   }
 
   req.session.user = { id: existingUser._id, email: existingUser.email }; /* creer les datas a stocker, express-session va les stocker automatiquement dans la db/collection*/
@@ -113,9 +141,9 @@ router.get('/admin', async function (req, res) {
     return res.status(401).render('401');
   };
 
-  const user =  await db.getDb().collection('users').findOne({_id: req.session.user.id});
-  if ( !user || !user.isAdmin) {
-   return res.status(403).render('403');
+  const user = await db.getDb().collection('users').findOne({ _id: req.session.user.id });
+  if (!user || !user.isAdmin) {
+    return res.status(403).render('403');
   };
 
   res.render('admin');
